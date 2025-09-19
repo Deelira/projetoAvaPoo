19d439726ec4ef0c13a15616e04394578003386b
Alisson Lira <3deelira@gmail.com>
2025-09-19 17:26:12 -0300

    subindo arquivos

---
+++ src/Veiculo.java
@@ -0,0 +1,27 @@
+public class Veiculo {
+
+    public String marca;
+    public String modelo;
+    public String placa;
+    public int ano;
+    public boolean disponivel;
+    public double preco;
+
+    public Veiculo (String marca, String modelo, String placa, int ano, double preco){
+        this.disponivel = true;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.placa = placa;
+        this.ano = ano;
+    }
+
+    public void mudarDisponibilidade (boolean disponibilidade){
+        this.disponivel = disponibilidade;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("<Veículo: Marca = %s, Modelo = %s, Placa = %s, Ano = %s, Disponivel = %b, Preço = $.2f>"
+                , this.marca, this.modelo, this.placa, this.ano, this.disponivel, this.preco);
+    }
+}
